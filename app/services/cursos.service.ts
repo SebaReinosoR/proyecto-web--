5740635a9969bf1416075d51f6b80528debbb21b
SebaReinosoR <91642529+SebaReinosoR@users.noreply.github.com>
2021-11-30 23:45:12 -0300

    Add files via upload

---
+++ app/services/cursos.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  constructor(private http:HttpClient) {
+     
+  }
+  HttpUploadOptions = {
+    headers: new HttpHeaders(
+      {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Content-Type': 'application/json',
+    }
+    ),
+  };
+
+  getCursos():Observable<any>{
+    return this.http.get(`${environment.hostname}/Cursos`);
+  }
+  postUsuario(nombre:any , apellido:any , rut:any , password:any , mail:any , telefono:any , pais:any , ciudad:any ):Observable<any>{
+    return this.http.post(`${environment.hostname}/AgregarUsuario`,JSON.stringify({"nombre":nombre,"apellido":apellido ,"rut":rut , "password":password , "mail":mail, "telefono":telefono , "pais":pais , "ciudad":ciudad}),this.HttpUploadOptions);
+  }
+  deleteUsuario(rut:any):Observable<any>{
+    return this.http.delete(`${environment.hostname}/EliminarUsuario/${rut}`,this.HttpUploadOptions);
+  }
+  putUsuario(rut:any,nombre:any):Observable<any>{
+    return this.http.put(`${environment.hostname}/ModificarUsuario/${rut}`,JSON.stringify({"nombre":nombre,"rut":rut}),this.HttpUploadOptions);
+  }
+  IniciarUsuario(mail:any,password:any):Observable<any>{
+    return this.http.get(`${environment.hostname}/Usuario/${mail}/${password}`,this.HttpUploadOptions);
+  }
+}
